38d8da5f41587ed649e2cdd240b1b2f79a391db5
louhao <louhao@gmail.com>
2013-01-25 21:16:40 +0800

    Initial Commit

---
+++ iWAserver/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  iWAserver
+//
+//  Created by louhao on 13-1-25.
+//  Copyright (c) 2013年 louhao. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
